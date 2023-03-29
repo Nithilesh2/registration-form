ddd176376ff542f4b9b4b02485506f1f6b123eff
Nithilesh2 <120772391+Nithilesh2@users.noreply.github.com>
2023-03-29 22:15:01 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+  const scriptURL = 'https://script.google.com/macros/s/AKfycby8W--so68dvPNHqRRn8QTSQ1nSkPObRhs0fnVDqhD07uMgfpdW1elNeS70aJP_-tWk/exec'
+  const form1 = document.forms['submit-to-google-sheet']
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => alert("form subbmitted successfully....!"))
+      .catch(error => console.error('Error!', error.message))
+  })
+
