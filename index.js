396a5b60fd01fb66382f7cbf11d2a5a5954bcefb
Nithilesh2 <120772391+Nithilesh2@users.noreply.github.com>
2023-03-29 23:00:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+
+
   const scriptURL = 'https://script.google.com/macros/s/AKfycby8W--so68dvPNHqRRn8QTSQ1nSkPObRhs0fnVDqhD07uMgfpdW1elNeS70aJP_-tWk/exec'
   const form1 = document.forms['submit-to-google-sheet']
 
   form.addEventListener('submit', e => {
     e.preventDefault()
+    onSubmit={(e)=>e.preventDefault()}
     fetch(scriptURL, { method: 'POST', body: new FormData(form)})
       .then(response => alert("form subbmitted successfully....!"))
       .catch(error => console.error('Error!', error.message))
